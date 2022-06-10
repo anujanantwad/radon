ab0c872182b92945078f84ddad9cc9b784cad05d
anujanantwad <101970630+anujanantwad@users.noreply.github.com>
2022-06-10 10:43:53 +0530

    populate ref

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,15 +1,35 @@
 const authorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
-
+const publisherModel=require("../models/publisherModel")
 const createBook= async function (req, res) {
     let book = req.body
+    let authorId=book.author_id
+    let publisherId=book.publisher_id
+// authorId validation===========================================================
+    if(!authorId){return res.send("author id required")}
+    
+    let author= await authorModel.findById(authorId)
+    if(!author) {return res.send("enter a valid author id")}
+  
+
+// publisherID validation==================================================
+    if(!publisherId) {return res.send("publisher id required")}
+    let publisher=await publisherModel.findById(publisherId)
+    if(!publisher){return res.send("enter a valid publisher id")}
+
+
     let bookCreated = await bookModel.create(book)
     res.send({data: bookCreated})
 }
-
+// =============================================
 const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
+    let books = await bookModel.find().populate('author_id publisher_id')
     res.send({data: books})
+}
+// =================
+const updatePublisher = async function (req, res) {
+    let updated = await bookModel.updateMany({name:{$or:[Penguin,HarperCollins]}},{$set:{isHardOver} })
+    res.send({data: specificBook})
 }
 
 const getBooksWithAuthorDetails = async function (req, res) {
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const authorController= require("../controllers/authorController")
 const bookController= require("../controllers/bookController")
+const publisherController=require("../controllers/publisherController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -10,7 +11,7 @@ router.get("/test-me", function (req, res) {
 
 router.post("/createAuthor", authorController.createAuthor  )
 
-router.get("/getAuthorsData", authorController.getAuthorsData)
+router.post("/createPublisher", publisherController.createPublisher)
 
 router.post("/createBook", bookController.createBook  )
 
