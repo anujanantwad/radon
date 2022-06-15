ab9d37cca7506ad2b9f08a13b5308d37dfad5925
anujanantwad <101970630+anujanantwad@users.noreply.github.com>
2022-06-15 23:46:09 +0530

    abc

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,7 +7,7 @@ const createUser = async function (abcd, xyz) {
   //the second parameter is always the response
   let data = abcd.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
+  console.log("user created");
   xyz.send({ msg: savedData });
 };
 
@@ -72,6 +72,7 @@ const updateUser = async function (req, res) {
 // Check if the token present is a valid token
 // Return a different error message in both these cases
 
+
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
   //Return an error if no user with the given id exists in the db
@@ -80,11 +81,24 @@ const updateUser = async function (req, res) {
   }
 
   let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId },userData,{new:true});
   res.send({ status: updatedUser, data: updatedUser });
 };
+const deletedUser= async function(req,res){
+  let userId=req.params.userId;
+  let user = await userModel.findById(userId)
+  
+  if(!user){
+    return res.send("no such user exist");
+  }
+let updatedUser=await userModel.findOneAndUpdate({_id:userId},{isDeleted:true},{new:true});
+res.send({status:true,updatedUser})
+if(updatedUser.isDeleted==true)
+{return res.send("user already deleted")};
+}
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deletedUser = deletedUser;
